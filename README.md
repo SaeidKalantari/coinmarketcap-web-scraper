8bc27ad913ff0ca4b4c1d4aee2c4a15b404de33f
Saeid <saeedka222.sk@gmail.com>
2022-04-22 15:52:04 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Cryptocurrency Web Scraper With Python - Coinmarketcap Web Scraper
+In this tutorial, I will show you how to build a Cryptocurrency Web Scraper With Python (It's fun ... trust me!)
+
+So crypto is booming and price prediction is what separates a good trader from other traders. But that is not the only way of making money with crypto. You can use Python web scrapping to gather Bitcoin, Ethereum, and other cryptocurrency prices. Even gather NFT or bitcoin news and sell them with an API or a bot. In this video, we'll learn how to build a Python web scraper for the Coinmarketcap online price-tracking website. 
+
+Github Link To The Code:  https://bit.ly/coinmarketcap-web-scraper
+
+__
+⭐️ You can get a blockchain domain with my affiliate link here: https://bit.ly/nft-crypto-domains
+
+⭐️ If you would like to buy me a coffee, well thank you very much that is mega kind! : https://www.buymeacoffee.com/saeidkai
+
+
+Follow us on social media:
+⇢ My LinkedIn: https://www.linkedin.com/in/saeid-kalantari
---
+++ coinmarketcap.py
@@ -0,0 +1,104 @@
+# Written by SAEID KALANTARI
+
+import json
+from requests_html import HTMLSession
+
+root_address = 'https://coinmarketcap.com'
+session = HTMLSession()
+
+def scrape_coins(data):
+    print('Scraping coins...')
+    pages = []
+    for page_number in range(1, 101):
+        pages.append(root_address + '/?page=' + str(page_number))
+
+    for page in pages:
+        print("Scrapping... page: " + str(page))
+
+        r = session.get(page)
+        r.html.render(scrolldown=16, sleep=0.1)
+
+        rows = r.html.find('tbody tr')
+
+        for row in rows:
+            d = dict()
+            columns = row.find('td')
+            if columns[1].find('p', first=True) is not None:
+                d['rank'] = columns[1].find('p', first=True).text.strip()
+                name_and_sym = columns[2].find('p')
+                d['name'] = name_and_sym[0].text.strip()
+                d['symbol'] = name_and_sym[1].text.strip()
+                price_col = columns[3].find('span')
+                d['price'] = price_col[0].text.strip()
+
+                daily_p = columns[4].find('span')
+                daily_span_neg = columns[4].find('.icon-Caret-down')
+                daily_span_pos = columns[4].find('.icon-Caret-up')
+                if daily_span_neg:
+                    daily_span_sym = '-'
+                elif daily_span_pos:
+                    daily_span_sym = '+'
+                if columns[4].text.strip() != '--':
+                    d['h24'] = daily_span_sym + daily_p[0].text.strip()
+                else:
+                    d['h24'] = '-'
+
+                weekly_p = columns[5].find('span')
+                weekly_span_neg = columns[5].find('.icon-Caret-down')
+                weekly_span_pos = columns[5].find('.icon-Caret-up')
+                if weekly_span_neg:
+                    weekly_span_sym = '-'
+                elif weekly_span_pos:
+                    weekly_span_sym = '+'
+                if columns[5].text.strip() != '--':
+                    d['h7'] = weekly_span_sym + weekly_p[0].text.strip()
+                else:
+                    d['h7'] = '-'
+
+                market_cap_col = columns[6].find('p')
+                if columns[6].text.strip() != '--':
+                    d['market_cap'] = market_cap_col[0].text.strip()
+                else:
+                    d['market_cap'] = '-'
+
+                volume_daily_col = columns[7].find('p')
+                if columns[7].text.strip() != '--':
+                    d['Volume_24h_dollar'] = volume_daily_col[0].text.strip()
+                    d['Volume_24h_sym'] = volume_daily_col[1].text.strip()
+                else:
+                    d['Volume_24h_dollar'] = '-'
+                    d['Volume_24h_sym'] = '-'
+
+                circulating_supply_col = columns[8].find('p')
+                if columns[8].text.strip() != '--':
+                    d['circulating_supply'] = circulating_supply_col[0].text.strip()
+                else:
+                    d['circulating_supply'] = '-'
+            data.append(d)
+
+    return data
+
+
+def save_json(data):
+    with open('coins.json', 'w') as f:
+        json.dump(data, f)
+
+
+def open_json():
+    with open('coins.json', 'r') as f:
+        return json.load(f)
+
+
+def main():
+    data = []
+    data = scrape_coins(data)
+    save_json(data)
+
+    print('Done.')
+
+
+if __name__ == '__main__':
+    main()
+
+
+
